96891dc05b36a7f607332a3e6e0cb0b81902fbac
Anass <AnassQ@users.noreply.github.com>
2014-07-19 01:59:36 +0000

    Update Anti_Spam.php

--- Admin-Block/Anti_Spam.php
+++ Admin-Block/Anti_Spam.php
@@ -1,4 +1,4 @@
-
+//Link : http://bit.ly/1qlIWo1
 function Anass_url_spamcheck( $approved , $commentdata ) {
     return ( strlen( $commentdata['comment_author_url'] ) > 50 ) ? 'spam' : $approved;
 }
